abf2723e790e16a4eb853c880e2f5d89d44531b8
juliana <Julianita12122005@outlook.es>
2023-09-21 15:23:35 -0500

    Todos los trabajos

---
+++ Corte2/Sesion13/Poo_listas.py
@@ -0,0 +1,15 @@
+class Pokemon():
+    def __init__(self):
+        self.color=None
+        self.categoria=None
+        self.tipo=None
+        self.codigo=None
+
+        def ataquePokemon(self):
+            return "Estoy ataque"
+       
+
+
+def main():
+    pass 
+
