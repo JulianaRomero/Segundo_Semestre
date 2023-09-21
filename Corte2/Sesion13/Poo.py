46e033ffa6c11891e9e6fea2e2a224be632db8a8
juliana <Julianita12122005@outlook.es>
2023-09-21 15:29:40 -0500

    Todos los trabajos

--- Corte2/Sesion13/Poo.py
+++ Corte2/Sesion13/Poo.py
@@ -1,44 +1,56 @@
+
 class Pokemon():
     def __init__(self):
+        self.nombre=None
         self.color=None
         self.categoria=None
         self.tipo=None
         self.codigo=None
-
-        def correr(self):
-            return "Estoy corriendo"
-        
-        def volor(self):
-            return"Estoy volando"
-        
-        def quemar(self):
-            return"Estoy quemando"
-
-
-def main()
-    Pikachu = Pokemon()
-    Pikachu.color = "Amarillo"
-    Pikachu.categoria = "Raton"
-    Pikachu.tipo = "Electrico"
-    Pikachu.codigo = 25
-    print(id(Pikachu))
-    print f"{Pikachu.nombre}:{Pikachu.correr()})
-
-    Charizard = Pokemon()
-    Charizard.color = "Naranja"
-    Charizard.categoria = "Dragon"
-    Charizard.tipo = "Fuego"
-    Charizard.codigo = 6
-    print(f"{Charizard.nombre}:{Charizard.volar()}")
-
-    Minetales = Pokemon()
-    Minetales.color = "Baige"
-    Minetales.categoria = "Zorro"
-    Minetales.tipo = "Fuego"
-    Minetales.codigo = 38
-    print(f"{Minetales.nombre}:{Minetales.quemar()}")
+        self.region='Colombia'
     
+    def correr(self):
+        return 'estoy corriendo'
+
+    def volar(self):
+        return 'estoy volando'
+    
+    def quemar(self):
+        return 'estoy quemando'
+    
+    def ataquePokemon(self):
+        return 'estoy atacando'
 
 
-if__name__==__"main"__
-        main()
+def main():
+    Pokemon1=Pokemon()
+    Pokemon1.nombre='Pikachu'
+    Pokemon1.color='Amarillo'
+    Pokemon1.categoria='Raton'
+    Pokemon1.tipo='Electrico'
+    Pokemon1.codigo=25
+    print(f'{Pokemon1.nombre}: {Pokemon1.correr()}\n'\
+    f'region: {Pokemon1.region}')
+
+    Charizard=Pokemon()
+    Charizard.nombre='Charizard'
+    Charizard.color='Naranja'
+    Charizard.categoria='Dragon'
+    Charizard.tipo='Fuego'
+    Charizard.codigo=6
+    print(f'{Charizard.nombre}: {Charizard.volar()}\n'\
+    f'region: {Charizard.region}')
+
+    Ninetales=Pokemon()
+    Ninetales.nombre='Ninetales'
+    Ninetales.color='Baige'
+    Ninetales.categoria='Zorro'
+    Ninetales.tipo='Fuego'
+    Ninetales.codigo=38
+    Ninetales.region='Peruano'
+    print(f'{Ninetales.nombre}: {Ninetales.quemar()}\n'\
+    f'region: {Ninetales.region}')
+
+
+if __name__=="__main__":
+    main()
+    
